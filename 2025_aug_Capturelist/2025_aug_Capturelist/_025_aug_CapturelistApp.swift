e2d4a4ca2e8070896ef00fec5187861b81f34951
sayalion <dltkdi173@gmail.com>
2025-08-22 15:11:23 +0900

    삭제 & 재생성

---
+++ 2025_aug_Capturelist/2025_aug_Capturelist/_025_aug_CapturelistApp.swift
@@ -0,0 +1,17 @@
+//
+//  _025_aug_CapturelistApp.swift
+//  2025_aug_Capturelist
+//
+//  Created by saya lee on 8/22/25.
+//
+
+import SwiftUI
+
+@main
+struct _025_aug_CapturelistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
